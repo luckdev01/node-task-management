e780941a5649768811e7ff5501357d8587ce5d39
denisdev10 <strongweb102@gmail.com>
2020-01-31 05:52:09 +0200

    add editing Board Title api

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const boardService = require('../services/board.service')
 
 router.get('/boards', boardService.getAll.bind(boardService))
 router.get('/boards/:boardId', boardService.getById.bind(boardService))
+router.put('/boards/:boardId', boardService.update.bind(boardService))
 
 module.exports = router
