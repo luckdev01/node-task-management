6966792c0976c81ee4b763f4ce503309868a8de2
denisdev10 <strongweb102@gmail.com>
2020-02-08 17:05:45 +0200

    change remote database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   remoteUrl:
-    'mongodb+srv://admin:mypwd@cluster0-wynew.mongodb.net/task-management-system?retryWrites=true&w=majority',
+    'mongodb://admin:mytaskpwd1@ds049347.mlab.com:49347/db-task-management',
   localUrl: 'mongodb://127.0.0.1:27017/task-management-system'
 }
